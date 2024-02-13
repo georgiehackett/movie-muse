fb995cac66c4aee47760e6e89195333c171b5842
Georgie <georgiehackett47@gmail.com>
2024-02-13 16:52:51 +0000

    Initialise repo

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  server: {
+    open: true,
+    port: 3000
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.js',
+  }
+})
