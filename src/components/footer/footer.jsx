13142941ef0305604d9a0ce4acb159439b7ee2c0
abdeeg <abdeegdahir@live.com>
2024-02-15 19:08:21 +0000

    last column placeholder completed for the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,13 +4,12 @@ import './footer.css'
 function Footer() {
     return (
         <>
-        <h1>Hi testing testing</h1>
         <nav className="navbar navbar-expand-lg bg-body-tertiary"></nav>
         <div className="Footer">
             <div className="container">
             <div className="row">
-            <div className="col-md-6 col-lg-5 col-12">
-                <a className="navbar-brand" href="/">Movie Muse</a>
+            <div className="col-md-6 col-lg-5 col-12 ft-1">
+                <a className="navbar-brand" href="/"><img src="" alt="" />Movie Muse</a>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque hic, accusantium veniam eveniet perferendis corrupti tenetur amet accusamus consequatur possimus quisquam eum labore placeat voluptatum iure, aspernatur, unde quasi perspiciatis.</p>
             <div className="footer-icons">
             <i class="fa-brands fa-facebook"></i>
@@ -18,8 +17,8 @@ function Footer() {
             <i class="fa-brands fa-linkedin"></i>
             </div>
             </div>
-            <div className="col-md-6 col-lg-3 col-12">
-            <h3>Quick Links</h3>
+            <div className="col-md-6 col-lg-3 col-12 ft-2">
+            <h5>Quick Links</h5>
                 <ul>
                     <li className="nav-item">
                         <a className="nav-link" href="/">Test 1</a>
@@ -35,8 +34,12 @@ function Footer() {
                     </li>
                 </ul>
             </div>
-            <div className="col-md-6 col-lg-4 col-12"></div>
-            
+            <div className="col-md-6 col-lg-4 col-12 ft-3">
+            <h5>Quick Links</h5>
+            <p><i class="fa-solid fa-phone"></i>+44 9823742935 235</p>
+            <p><i class="fa-solid fa-envelope"></i>aksgfasfhasf@gmail.com</p>
+            <p><i class="fa-solid fa-location-dot"></i>Birmingham, England</p>
+            </div>
             </div>
             </div>
         </div>
