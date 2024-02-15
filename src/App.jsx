f7e60c3339ff85068adeb7e63e3f6b3bdb985b6b
AngelikaWidz <angelikawidz@yahoo.com>
2024-02-15 19:41:20 +0000

    removed links

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from './navbar/navbar';
+import { Router } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <h1>Main Content Here</h1>
     </div>
   );
 };
 
 export default App;
+// set up links
+// paths set up
+// stykibng - 
--- src/navbar/navbar.jsx
+++ src/navbar/navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // If you are using React Router
-
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
@@ -8,17 +7,17 @@ const Navbar = () => {
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Home</Link>
+        <div className="navbar-brand" to="/">Home</div>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <div className="nav-link" to="/about">About</div>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <div className="nav-link" to="/contact">Contact</div>
             </li>
           </ul>
         </div>
@@ -29,3 +28,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+// set up router -link part
