0923b4d5deb605a829604c157703601c36a29e95
Your Name <you@example.com>
2024-02-16 19:33:33 +0000

    Adding Popular movies to Front page

--- src/Movie/MovieCard.jsx
+++ src/Movie/MovieCard.jsx
@@ -1,24 +1,29 @@
 import React,{useEffect, useState} from "react";
-
+const API_KEY="131c856f75867823ef322849c2612110";
 const Image_url = "https://image.tmdb.org/t/p/w200/";
+const Popular_url = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${API_KEY}`;
+//const Search_url= `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=${process.env.API_KEY}&query=`
 function MovieCards() {
     const [movieList,setMovieList] = useState([])
     const getMovie = ()=>{
 
-        fetch("https://api.themoviedb.org/3/discover/movie?api_key=131c856f75867823ef322849c2612110")
+        fetch(`${Popular_url}`)
        .then(res => res.json())
        .then(json => setMovieList(json.results))
     }
     useEffect(()=>{
         getMovie()
     },[])
-     console.log(movieList);
+     
     return(
         <>
+        <div>
+            <h1>Popular Movies</h1>
+            </div>
     <div className="CardDisplay">
         {movieList.map((movie)=>(
             <div className="movieCardContainer" key={movie.id}>
-              <img  src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+              <img  src={Image_url + `${movie.poster_path}`} alt={movie.title} />
                <h3 className="CardTitle">{movie.title}</h3>
             <h5>{movie.release_date}</h5>
         
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig , loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( {
   plugins: [
     react()
   ],
