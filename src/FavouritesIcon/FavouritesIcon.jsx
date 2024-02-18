3d159a205e8571999830127efda0ee429aa13307
Georgie <georgiehackett47@gmail.com>
2024-02-18 23:31:37 +0000

    Fix typo in FavouritesIcon component

--- src/FavouritesIcon/FavouritesIcon.jsx
+++ src/FavouritesIcon/FavouritesIcon.jsx
@@ -6,7 +6,7 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 function FavouritesIcon() {
   return (
     <>
-      <span id="favourites-text">Add to Favourties</span>
+      <span id="favourites-text">Add to Favourites</span>
       <FontAwesomeIcon icon={faHeart} />
     </>
   );
