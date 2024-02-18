b63026191b98ea504100c06b1215675ec0b18916
Georgie <georgiehackett47@gmail.com>
2024-02-18 23:41:04 +0000

    Declare state variable favouriteMovies

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import './App.css';
 import FavouritesIcon from './FavouritesIcon/FavouritesIcon';
 
 function App() {
-  console.log(FavouritesIcon);
+  const [favouriteMovies, setFavouriteMovies] = useState([]);
+
   return (
     <>
       <Moviecard favouriteComponent={FavouritesIcon} />
