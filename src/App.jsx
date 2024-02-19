6a26b7679527cb622e77da43b5b9684c07cd1a76
Your Name <you@example.com>
2024-02-19 22:26:45 +0000

    Getting the horizontal scroll feature to the Popular movies section working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-
 import Navbar from './navbar/navbar'; 
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import React from 'react';
 import Hero from './Hero/Hero';  
 import Footer from './footer/footer'; 
- import MovieCard from './Movie/MovieCard'; 
+ import PopularMovie from './Movie/PopularMovie'; 
 
 import './App.css';
 import MovieDetails from './Movie/MovieDetails';
@@ -17,7 +16,7 @@ const App = () => {
        <Hero />
   
   <Routes>
-      <Route path="/" element={<MovieCard />} />
+      <Route path="/" element={<PopularMovie />} />
       {/* <Route path="/popularmovies" element={<PopularMovies />} /> */}
       <Route path='/moviedetail/:id' element={<MovieDetails />} />
     </Routes> 
