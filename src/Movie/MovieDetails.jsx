ba9e1c67456df73196f0a7b1f7be452cf7e39c76
Your Name <you@example.com>
2024-02-19 17:51:20 +0000

    Rendered movie poster and overview to movie detail page

--- src/Movie/MovieDetails.jsx
+++ src/Movie/MovieDetails.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useParams,Link } from 'react-router-dom'
 import { HiChevronLeft } from "react-icons/hi";
-import noimage from '../images/no-image.jpg'
+import noimage from '../assets/images/no-image.jpg';
 import { FaPlay } from "react-icons/fa";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import slugify from 'react-slugify';
 
 const API_KEY="131c856f75867823ef322849c2612110";
@@ -14,26 +16,28 @@ const MovieDetails = () => {
     const [moviedet, setMoviedet] = useState([]);
   const [castdata, setCastdata] = useState([]);
   const [moviegenres, setMoviegenres] = useState([]);
-  
-    
+  const [video, setVideo] = useState([]);
+
   const fetchMovie = async () => {
     const data = await fetch(
       `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
     );
     const moviedetail = await data.json();
     setMoviedet(moviedetail);
-    // console.log(moviedetail);
+    //console.log(moviedetail);
     setMoviegenres(moviedetail.genres);
-    setLoader(false);
+    
   };
 
   const fetchCast = async () => {
     const castdata = await fetch(
+        
       `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language`
     );
     const castdetail = await castdata.json();
+    //console.log(castdetail);
     setCastdata(castdetail.cast);
-    
+   
   }
 
   const fetchVideo = async () => {
@@ -42,7 +46,7 @@ const MovieDetails = () => {
     );
     const videodata = await data.json();
     setVideo(videodata.results);
-    // console.log(videodata.results);
+    //console.log(videodata.results);
   }
 
   useEffect(() => {
@@ -55,36 +59,31 @@ const MovieDetails = () => {
   return (
 
     <>
-      {
-        loader ? <div className='h-screen w-full flex justify-center items-center'><span className="loader m-10"></span></div> :
-          <>
-
-            <Link to="/" className='fixed z-10 text-4xl text-black bg-white m-3 md:m-5 rounded-full'><HiChevronLeft /></Link>
-
             {/* poster */}
+            {/* <h1 className='text-black text-center'>{moviedet.title}</h1> */}
             <div className='relative h-auto md:h-[82vh] flex justify-center'>
-              <div className='h-full w-full shadowbackdrop absolute'></div>
-              <h1 className='text-white absolute bottom-0 p-10 text-2xl md:text-6xl font-bold text-center'>{moviedet.title}</h1>
-              {moviedet.backdrop_path === null ? <img src={noimage} className='h-full w-full' /> : <img src={"https://image.tmdb.org/t/p/original/" + moviedet.backdrop_path} className='h-full w-full' />}
+             {/*  <div className='h-full w-full shadowbackdrop absolute'></div> */}
+              <h1 className='text-black text-center'>{moviedet.title}</h1>
+              {moviedet.backdrop_path === null ? <img src={noimage} className='img-fluid rounded' /> : <img src={"https://image.tmdb.org/t/p/original/" + moviedet.backdrop_path} className='img-fluid rounded' />}
             </div>
-
+   
             {/* overview */}
-            <h2 className='text-white text-center pt-5 px-3 md:px-60 font-Roboto text-[18px]'>{moviedet.overview}</h2>
+            <h2 className='text-black text-center pt-5 px-3 md:px-60 font-Roboto text-[18px]'>{moviedet.overview}</h2>
 
             <div className='text-blue-100 font-semibold my-3 flex justify-center'>
               <h2 className='bg-blue-600/30 border-2 border-blue-700 py-2 px-3 rounded-full'>Release Date : {moviedet.release_date}</h2>
             </div>
 
-            {/* tag */}
-            <div className='flex justify-center flex-wrap'>
+            {/* tag 
+            <div className='flex justify-center flex-wrap' >
               {moviegenres.map((tag) => (
                 <>
                   <div key={tag.id} className='text-white font-semibold bg-gray-800 rounded-full px-4 py-1 m-2'>{tag.name}</div>
                 </>
               ))}
-            </div>
+            </div>*/}
 
-            {/* cast */}
+            {/* cast 
             <div className='flex flex-col items-center'>
               <h1 className="text-3xl text-blue-300 font-semibold text-center p-2">Cast</h1>
 
@@ -102,9 +101,9 @@ const MovieDetails = () => {
                   </>
                 ))}
               </div>
-            </div>
+            </div>*/}
 
-            {/* trailer */}
+            {/* trailer 
             <div className='flex justify-center items-center mb-10 gap-5 flex-wrap'>
               {Array.from(video).filter(trail => trail.type === "Trailer").map((trail, index) => (
                 <>
@@ -116,17 +115,18 @@ const MovieDetails = () => {
                 </>
               ))
               }
-            </div>
+            </div>*/}
 
-            {/* watch movie */}
+            {/* watch movie 
             <div className='flex justify-center items-center mb-10 gap-5 flex-wrap'>
               <Link  to={`/player/${id}/${slugify(moviedet.title)}`} className='flex border-2 border-green-600 bg-green-600/40 p-3 items-center justify-center gap-2 text-xl font-semibold rounded-full text-white'>
                 <FaPlay />Watch Movie
               </Link>
-            </div>
+            </div>*/}
+          
           </>
-      }
-    </>
+     
+    
   )
 }
 
