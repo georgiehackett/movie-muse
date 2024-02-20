0e07b370f935c78e813b89adee6a2ae7adbeab62
victorlee <victor_lee109@hotmail.co.uk>
2024-02-20 22:16:16 +0000

    Logo image added to Hero component

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -1,32 +1,32 @@
-import React,{useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 import './Hero.css';
-import MovieCard from "../Movie/MovieCard";
+import MovieCard from '../Movie/MovieCard';
 
-const API_KEY="131c856f75867823ef322849c2612110";
+const API_KEY = '131c856f75867823ef322849c2612110';
 
 const JumbotronSearch = () => {
-  const [query, setQuery] = useState(""); 
-  const [results,setResult] = useState([]);
+  const [query, setQuery] = useState('');
+  const [results, setResult] = useState([]);
 
- 
-  const onChange = (e) =>{
+  const onChange = e => {
     e.preventDefault();
-    const Search_url= `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=${API_KEY}&query=${e.target.value}`
+    const Search_url = `https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1&api_key=${API_KEY}&query=${e.target.value}`;
     setQuery(e.target.value);
 
     fetch(`${Search_url}`)
-       .then(res => res.json())
-       .then(data =>{ 
-        if(!data.errors) {
-          setResult(data.results)
+      .then(res => res.json())
+      .then(data => {
+        if (!data.errors) {
+          setResult(data.results);
           //console.log(data.results);
         }
-        });
+      });
   };
 
   return (
     <div className="jumbotron">
-      <h1 className="display-4">Welcome to Movie Muse</h1>
+      {/* <h1 className="display-4">Welcome to Movie Muse</h1> */}
+      <img src="src/images/logo.png" alt="logo" />
       <p className="lead">Find your favourite movies here!</p>
       <hr className="my-4" />
       <div className="input-group mb-3">
@@ -37,14 +37,8 @@ const JumbotronSearch = () => {
           value={query}
           onChange={onChange}
         />
-        {/* <button className="btn btn-primary" type="button">
-          Search
-        </button> */}
       </div>
-      {results.length >0 && (
-      <MovieCard movieList={movieList}/>
-      )
-      }
+      {results.length > 0 && <MovieCard movieList={movieList} />}
     </div>
   );
 };
