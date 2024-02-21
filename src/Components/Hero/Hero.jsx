b8804b8c166139608d2a1d1109a4f86de1794aaf
Georgie <georgiehackett47@gmail.com>
2024-02-21 23:32:18 +0000

    Render search results outside jumbotron div

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,15 +3,12 @@ import './Hero.css';
 import MovieCard from '../Movie/MovieCard';
 import Logo from '../assets/images/logo-recolour.png';
 
-
-
-const API_KEY="131c856f75867823ef322849c2612110";
+const API_KEY = '131c856f75867823ef322849c2612110';
 
 const JumbotronSearch = () => {
-  const [query, setQuery] = useState(""); 
-  const [results,setResult] = useState([]);
+  const [query, setQuery] = useState('');
+  const [results, setResult] = useState([]);
   //const [movieList,setMovieList] = useState([])
-
 
   const onChange = e => {
     e.preventDefault();
@@ -27,7 +24,7 @@ const JumbotronSearch = () => {
         }
       });
   };
- /*  const getMovie = ()=>{
+  /*  const getMovie = ()=>{
 
     fetch(`${Search_url}`)
    .then(res => res.json())
@@ -37,25 +34,23 @@ useEffect(()=>{
     getMovie()
 },[]) */
   return (
-    <div className="jumbotron">
-      {/* <h1 className="display-4">Welcome to Movie Muse</h1> */}
-      <img src={Logo} alt="logo" id='logo' />
-      <p className="lead">Find your favourite movies here!</p>
-      <hr className="my-4" />
-      <div className="input-group mb-3 d-flex justify-content-center">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search for movies..."
-          value={query}
-          onChange={onChange}
-        />
+    <>
+      <div className="jumbotron">
+        <img src={Logo} alt="logo" id="logo" />
+        <p className="lead">Find your favourite movies here!</p>
+        <hr className="my-4" />
+        <div className="input-group mb-3 d-flex justify-content-center">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for movies..."
+            value={query}
+            onChange={onChange}
+          />
+        </div>
       </div>
-      {results.length >0 && (
-      <MovieCard movieList={results}/>
-      )
-      }
-    </div>
+      {results.length > 0 && <MovieCard movieList={results} />}
+    </>
   );
 };
 
